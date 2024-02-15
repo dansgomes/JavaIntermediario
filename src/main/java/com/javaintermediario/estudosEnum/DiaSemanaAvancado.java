e95baec4d63259d5d550f0d202463534d37e68e1
Daniel <danielsgomes99@gmail.com>
2024-02-15 16:54:17 -0300

    Estudos Java sobre as classes Enum e sua diferença em utilização em comparação a constantes.

---
+++ src/main/java/com/javaintermediario/estudosEnum/DiaSemanaAvancado.java
@@ -0,0 +1,12 @@
+package com.javaintermediario.estudosEnum;
+
+public enum DiaSemanaAvancado {
+
+    SEGUNDA, TERCA, QUARTA, QUINTA, SEXTA, SABADO, DOMINGO;
+
+    private int valor;
+
+    DiaSemana(int valor){
+        this.valor = valor;
+    }
+}
