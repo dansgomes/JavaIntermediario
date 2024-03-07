93d3ef3d1538c5dc99d8c245b1b3024cdedde1e2
Daniel Souza <danielsgomes99@gmail.com>
2024-03-06 21:44:21 -0300

    Estudo sobre anotações em Java e como programadores usam constantemente em seus projetos reais como Springboot e Hibernate, etc.

---
+++ src/main/java/com/javaintermediario/estudosAnotacoes/Teste.java
@@ -0,0 +1,18 @@
+package com.javaintermediario.estudosAnotacoes;
+@InformacaoAula(
+        autor = "Loiane",
+        aulaNumero = 65,
+        blog = "loiane.com"
+)
+public class Teste {
+
+    @InformacaoAula(
+            autor = "Loiane",
+            aulaNumero = 65,
+            blog = "loiane.com"
+    )
+    public static void main(String [] args){
+
+
+    }
+}
