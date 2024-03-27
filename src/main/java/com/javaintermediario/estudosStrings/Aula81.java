ab8fa808e82be9698ee69c760e1e84bfaf8f0102
Daniel de Souza Gomes <103148478+dansgomes@users.noreply.github.com>
2024-03-26 21:41:56 -0300

    Estudo Sobre Strings e dessa vez sobre os métodos toLowerCase e toUpperCase.

---
+++ src/main/java/com/javaintermediario/estudosStrings/Aula81.java
@@ -0,0 +1,18 @@
+package com.javaintermediario.estudosStrings;
+
+public class Aula81 {
+    public static void main(String[] args){
+
+        String teste = "Teste";
+
+        String testeMinusc = teste.toLowerCase();
+        String testeMaiusc = teste.toUpperCase();
+
+        System.out.println(teste.toLowerCase());
+
+        System.out.println(testeMinusc);
+        System.out.println(testeMaiusc);
+
+        //if (teste.toLowerCase().equals(teste.toLowerCase()))
+    }
+}
