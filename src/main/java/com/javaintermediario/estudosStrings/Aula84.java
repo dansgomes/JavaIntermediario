3f418fe5384baf70afe1f939feae87e08b05813e
Daniel de Souza Gomes <103148478+dansgomes@users.noreply.github.com>
2024-04-01 17:21:29 -0300

    Estudo Sobre Strings e dessa vez sobre os métodos StringTokenizer.

---
+++ src/main/java/com/javaintermediario/estudosStrings/Aula84.java
@@ -0,0 +1,16 @@
+package com.javaintermediario.estudosStrings;
+
+import java.util.StringTokenizer;
+
+public class Aula84 {
+    public static void main (String[] args){
+
+        String doArquivo = "1;Antônio;30";
+
+        StringTokenizer st = new StringTokenizer(doArquivo, ";");
+
+        while (st.hasMoreTokens()){
+            System.out.println(st.nextToken());
+        }
+    }
+}
